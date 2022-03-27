7db1b219bc10e0342ecd7fc494b031a7427b5e38
Ezgi <erdemir_ezgi@hotmail.com>
2022-03-26 22:07:25 -0400

    new commit 1

---
+++ class1.java
@@ -0,0 +1,15 @@
+public class class1 {
+
+    public static void main(String[] args) {
+
+
+        /**
+         *
+         * git commit -m "first commit"
+         * git branch -M main
+         * git remote add origin https://github.com/IezgiI/IezgiI.git
+         * git push -u origin main
+         */
+
+    }
+}
